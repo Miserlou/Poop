e359f69cd130ceee070fc98539f232d9902d1421
Rich Jones <rich@anomos.info>
2017-08-25 16:39:22 -0400

    show poop works a bit better

--- poop/page-eater.js
+++ poop/page-eater.js
@@ -1,6 +1,26 @@
+// var backup = "";
+
+// var poop_script = "function showPoop(){ document.body.innerHTML = backup;}";
+// var poop_string = '<div style="display: none">POOP</div>';
+
+// var Base64={_keyStr:"ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/=",encode:function(e){var t="";var n,r,i,s,o,u,a;var f=0;e=Base64._utf8_encode(e);while(f<e.length){n=e.charCodeAt(f++);r=e.charCodeAt(f++);i=e.charCodeAt(f++);s=n>>2;o=(n&3)<<4|r>>4;u=(r&15)<<2|i>>6;a=i&63;if(isNaN(r)){u=a=64}else if(isNaN(i)){a=64}t=t+this._keyStr.charAt(s)+this._keyStr.charAt(o)+this._keyStr.charAt(u)+this._keyStr.charAt(a)}return t},decode:function(e){var t="";var n,r,i;var s,o,u,a;var f=0;e=e.replace(/[^A-Za-z0-9\+\/\=]/g,"");while(f<e.length){s=this._keyStr.indexOf(e.charAt(f++));o=this._keyStr.indexOf(e.charAt(f++));u=this._keyStr.indexOf(e.charAt(f++));a=this._keyStr.indexOf(e.charAt(f++));n=s<<2|o>>4;r=(o&15)<<4|u>>2;i=(u&3)<<6|a;t=t+String.fromCharCode(n);if(u!=64){t=t+String.fromCharCode(r)}if(a!=64){t=t+String.fromCharCode(i)}}t=Base64._utf8_decode(t);return t},_utf8_encode:function(e){e=e.replace(/\r\n/g,"\n");var t="";for(var n=0;n<e.length;n++){var r=e.charCodeAt(n);if(r<128){t+=String.fromCharCode(r)}else if(r>127&&r<2048){t+=String.fromCharCode(r>>6|192);t+=String.fromCharCode(r&63|128)}else{t+=String.fromCharCode(r>>12|224);t+=String.fromCharCode(r>>6&63|128);t+=String.fromCharCode(r&63|128)}}return t},_utf8_decode:function(e){var t="";var n=0;var r=c1=c2=0;while(n<e.length){r=e.charCodeAt(n);if(r<128){t+=String.fromCharCode(r);n++}else if(r>191&&r<224){c2=e.charCodeAt(n+1);t+=String.fromCharCode((r&31)<<6|c2&63);n+=2}else{c2=e.charCodeAt(n+1);c3=e.charCodeAt(n+2);t+=String.fromCharCode((r&15)<<12|(c2&63)<<6|c3&63);n+=3}}return t}}
+
 function eatPage() {
-    var withPoop = window.location.href + '#nopoop'
-    document.body.innerHTML = "<html><center><h1 style='font-size: 12rem; margin-top: 10%' onclick='history.go(-1)'><a href='javascript:history.back()'>💩</a></h1><h2 style='font-family: Helvetica; font-size:4rem; padding:2rem;'><a href='javascript:history.back()'>Go back</a></h2><p><a href='" + withPoop + "'>Show me the poop</a>.<center></html>";
+
+    // backup = document.body.innerHTML;
+    // // var b64poop = btoa(backup);
+    // var b64poop = Base64.encode(backup);
+
+    // if (document.body.innerHTML.includes(poop_string)){
+    //     return;
+    // }
+
+    var urlWithPoop = window.location.href + '#showpoop';
+    console.log(urlWithPoop);
+
+    document.body.innerHTML = "<html><body><center><h1 style='font-size: 12rem; margin-top: 10%' onclick='history.go(-1)'><a href='javascript:history.back()'>💩</a></h1><h2 style='font-family: Helvetica; font-size:4rem; padding:2rem;'><a href='javascript:history.back()'>Go back</a></h2><p><a href='#showpoop' onclick='location.reload()'>Show me the poop</a>.<center></body></html>";
+    //document.body.innerHTML += "<div style='z-index:100; width: 100%; height: 100%; top: 0; left: 0; position: absolute; background: white;'><center><h1 style='font-size: 12rem; margin-top: 10%' onclick='history.go(-1)'><a href='javascript:history.back()'>💩</a></h1><h2 style='font-family: Helvetica; font-size:4rem; padding:2rem;'><a href='javascript:history.back()'>Go back</a></h2><p><a href='" + withPoop + "'>Show me the poop</a>.<center></div>";
+    //document.body.innerHTML = "<div style='z-index:100; width: 100%; height: 100%; top: 0; left: 0; position: absolute; background: white;'><center><h1 style='font-size: 12rem; margin-top: 10%' onclick='history.go(-1)'><a href='javascript:history.back()'>💩</a></h1><h2 style='font-family: Helvetica; font-size:4rem; padding:2rem;'><a href='javascript:history.back()'>Go back</a></h2><p><a href='" + urlWithPoop + "'>Show me the poop</a>.<center></div>";
 }
 
 eatPage();
--- poop/poop.js
+++ poop/poop.js
@@ -20,11 +20,13 @@ let bad_domains = [
     "wwwadwordsselect.com"
 ];
 
+var blockPoop = true;
+
 function filterURL(requestDetails) {
   for (let domain of bad_domains) {
 
     // Filter the Poop.
-    if (requestDetails.url.includes(domain)){
+    if (requestDetails.url.includes(domain) && blockPoop){
         browser.tabs.executeScript({
           file: "page-eater.js",
           runAt: "document_end"
@@ -33,8 +35,58 @@ function filterURL(requestDetails) {
   }
 }
 
+// function listener(details) {
+//   console.log("onBeforeNavigate to: " + details.url);
+// }
+
 browser.webRequest.onBeforeRequest.addListener(
   filterURL, {
     urls: ["<all_urls>"]
   }
 );
+
+// browser.webNavigation.onCompleted.addListener(evt => {
+
+//     // Filter out any sub-frame related navigation event
+//     // if (evt.frameId !== 0) {
+//     //   return;
+//     // }
+
+//     console.log("ONCOMPLETED");
+//     const url = new URL(evt.url);
+//     console.log(url.hostname);
+
+//     // hostNavigationStats[url.hostname] = hostNavigationStats[url.hostname] || 0;
+//     // hostNavigationStats[url.hostname]++;
+
+//     // // Persist the updated stats.
+//     // browser.storage.local.set(results);
+//   }, {
+//     url: ["<all_urls>"]
+// });
+
+function handleUpdated(tabId, changeInfo, tabInfo) {
+  // console.log("Updated tab: " + tabId);
+  // console.log("Changed attributes: ");
+  // console.log(changeInfo);
+  // console.log("New tab Info: ");
+  // console.log(tabInfo);
+
+  if (changeInfo.url) {
+    if(changeInfo.url.includes("#showpoop")){
+        blockPoop = false;
+    } else{
+        blockPoop = true;
+    }
+  }
+
+}
+
+browser.tabs.onUpdated.addListener(handleUpdated);
+
+
+// browser.webNavigation.onBeforeNavigate.addListener(
+//   listener, {
+//     urls: ["<all_urls>"]
+//   }                      // optional object
+// )
